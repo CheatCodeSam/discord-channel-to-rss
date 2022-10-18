6014d1ff394c7dbaffa369ceb28fc4f22ea49c69
Carson Weeks <mail@carsonweeks.com>
2022-10-17 18:44:01 -0700

    Added waitress

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template
 from loguru import logger
 from models import Event, db, Announcement
 from discord_client import client
+from waitress import serve
+
+
 
 
 app = Flask(__name__)
@@ -36,7 +39,7 @@ def events():
     return json.dumps(events, indent=4, sort_keys=True, default=str)
 
 
-flask_thread(func=lambda: app.run())
+flask_thread(func=lambda: serve(app, host="0.0.0.0", port=8088))
 
 
 client.run("MTAyOTkxMDg4NTU3MDk5MDEzMA.GzgQQ7.xUGsI02u9XKY4d6tSz7756uXqBXYpKx7A2mEC4")

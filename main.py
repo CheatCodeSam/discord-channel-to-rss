c5439b46525f825b95a27982d9b9167a26f6ea2f
Carson Weeks <mail@carsonweeks.com>
2022-10-17 19:13:29 -0700

    Using env for variables.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from models import Event, db, Announcement
 from discord_client import client
 from waitress import serve
 from flask_cors import CORS
-
+import os
 
 
 
@@ -41,7 +41,7 @@ def events():
     return json.dumps(events, indent=4, sort_keys=True, default=str)
 
 
-flask_thread(func=lambda: serve(app, host="0.0.0.0", port=8888))
+flask_thread(func=lambda: serve(app, host=os.getenv("HOST", "0.0.0.0"), port=os.getenv("PORT", "8888")))
 
 
-client.run("MTAyOTkxMDg4NTU3MDk5MDEzMA.GzgQQ7.xUGsI02u9XKY4d6tSz7756uXqBXYpKx7A2mEC4")
+client.run(os.getenv("DISCORD_KEY"))
